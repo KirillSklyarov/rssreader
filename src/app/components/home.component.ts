3563212805f4eb099ceb2decf015c75706a24059
Kirill Sklyarov <kirill.sklyarov@gmail.com>
2017-07-04 14:39:34 +0300

    Try request local rss

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -9,13 +9,32 @@ import { Channel } from './../rss/channel'
   styleUrls: ['./../styles/style.css'],
   providers: [HttpService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  feed = require('rss-to-json')
   channels: Channel[] = []
+  channelList: string[] = ['./../public/lentanews.xml',
+    './../public/rbcnews.xml']
   channel: string = ''
 
   constructor (private httpService: HttpService) { }
 
+  ngOnInit () {
+    for (let i = 0; i < this.channelList.length; i++) {
+      this.httpService.getData(this.channelList[i]).subscribe((data: Response) =>
+      {
+        this.feed.load(this.channelList[i], function(err, rss) {
+            console.log(rss)
+        })
+      })
+    }
+  }
+
+  addChannel () {
+    this.httpService.getData(this.channel).subscribe((data: Response) => {
+      console.log(data.toLocaleString)
+    })
+  }
 
   isListEmpty (): boolean {
     if (this.channels.length > 0) { return true }
