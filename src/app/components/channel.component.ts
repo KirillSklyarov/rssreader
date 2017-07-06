6728bd3c8136555f438622770a3a018a69cc4bb2
Kirill Sklyarov <kirill.sklyarov@gmail.com>
2017-07-06 14:11:58 +0300

    Find channel in ChannelComponent

--- src/app/components/channel.component.ts
+++ src/app/components/channel.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Response } from '@angular/http'
 import { HttpService } from './../services/http.service'
+
+import { ActivatedRoute} from '@angular/router'
+import { Subscription } from 'rxjs/Subscription'
 
 import { Channel } from './../rss/channel'
 import { Item } from './../rss/item'
@@ -8,20 +11,51 @@ import { Image } from './../rss/image'
 import { TextInput } from './../rss/textinput'
 
 @Component({
-  selector: 'home-app',
+  selector: 'channel-app',
   templateUrl: './../pages/channel.html',
   styleUrls: ['./../styles/style.css'],
   providers: [HttpService]
 })
-export class ChannelComponent implements OnInit {
+export class ChannelComponent implements OnInit, OnDestroy {
 
-  channel: Channel
   channelUrl: string
+  channel: Channel
+  channelNotFound = true
 
-  constructor (private httpService: HttpService) { }
+  private channelName: string
+  private subscription: Subscription;
 
-  ngOnInit () {
 
+  constructor(private httpService: HttpService,
+    private activateRoute: ActivatedRoute){
   }
+
+  ngOnInit() {
+    this.subscription = this.activateRoute.params.subscribe(params => {
+      this.channelName = params['channel']
+      this.httpService.getData('assets/data/rsschannels.json').
+        subscribe((data: Response) => {
+          let channelList = data.json()
+
+          // Find current channel
+          channelList.forEach((channel, index) => {
+            if (this.channelName == channel.name) {
+              this.channelNotFound = false
+              
+            }
+          })
+        })
+    })
+  }
+
+  ngOnDestroy(){
+      this.subscription.unsubscribe();
+  }
+
+
+  // ngOnInit () {
+  //
+  // }
+
 
 }
