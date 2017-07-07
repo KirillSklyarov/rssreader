dfaf473bea030f549f01f57c9164b804b6992a35
Kirill Sklyarov <kirill.sklyarov@gmail.com>
2017-07-07 13:02:18 +0300

    Separate different channels in data

--- src/app/libs/feedsdatabaselink.ts
+++ src/app/libs/feedsdatabaselink.ts
@@ -1,1 +1,3 @@
-export const FEEDS_DATABASE_LINK = 'assets/data/rsschannels.json'
+export const FEEDS_DATABASE_LINK = 'assets/data/allchannels.json'
+// export const FEEDS_DATABASE_LINK = 'assets/data/englishchannels.json'
+// export const FEEDS_DATABASE_LINK = 'assets/data/russianchannels.json'
